48ec5451c6f99d35de78b39c4b5f87575903aabc
Andrej Dakic <andrej27.dakic@gmail.com>
2024-03-06 01:45:30 +0100

    responsive

--- script.js
+++ script.js
@@ -14,11 +14,28 @@ function showDiv(event, divNumber) {
   }
 
   const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      }
+    },
+
     loop: true,
     //mousewheel: true,
-    spaceBetween: 0,
-    effect: 'fade',
-    speed: 1000,
 
     navigation: {
       nextEl: ".swiper-button-next",
@@ -28,5 +45,18 @@ function showDiv(event, divNumber) {
     pagination: {
       el: '.swiper-pagination',
     },
-
+    
   });
+
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let navigationItems = document.querySelector('#navigation');
+
+const hamburgerEvent = (navigation, close, open) => {
+  navigationItems.style.display = navigation;
+  closeHam.style.display = close;
+  openHam.style.display = open;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
